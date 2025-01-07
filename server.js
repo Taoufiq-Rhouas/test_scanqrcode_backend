0ec3f3c718125b69e9e1d6c79d812f2597dc66f9
Taoufiq-Rhouas <rhouastaoufiq@gmail.com>
2025-01-07 12:08:18 +0100

    commit_V2_3

--- server.js
+++ server.js
@@ -1,81 +1,3 @@
-// // VERSION_1
-// const express = require('express');
-// const http = require('http');
-// const socketIo = require('socket.io');
-
-// const app = express();
-// const server = http.createServer(app);
-// const io = socketIo(server);
-
-// app.use(express.static('build'));
-
-// io.on('connection', (socket) => {
-//   console.log('A user connected');
-  
-//   socket.on('qr-scanned', (orderId) => {
-//     console.log('Order ID scanned:', orderId);
-//     io.emit('order-details', orderId); // Send order details to all clients
-//   });
-
-//   socket.on('disconnect', () => {
-//     console.log('A user disconnected');
-//   });
-// });
-
-// // server.listen(5000, () => {
-// //   console.log('Server running on http://localhost:5000');
-// // });
-
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-
-
-
-
-
-// // VERSION_2
-// const express = require('express');
-// const http = require('http');
-// const socketIo = require('socket.io');
-
-// const app = express();
-// const server = http.createServer(app);
-// const io = socketIo(server);
-
-// app.use(express.static('build'));
-
-// // Root route to check if the server is running
-// app.get('/', (req, res) => {
-//   res.send('Server is running and ready!');
-// });
-
-// io.on('connection', (socket) => {
-//   console.log('A user connected');
-  
-//   socket.on('qr-scanned', (orderId) => {
-//     console.log('Order ID scanned:', orderId);
-//     io.emit('order-details', orderId); // Send order details to all clients
-//   });
-
-//   socket.on('disconnect', () => {
-//     console.log('A user disconnected');
-//   });
-// });
-
-// // Use the PORT environment variable if available, otherwise default to 5000
-// const PORT = process.env.PORT || 5000;
-// server.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
-
-
-
-
-
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
@@ -132,7 +54,7 @@ app.post('/api/test', (req, res) => {
         console.log('====================================');
         // console.log("Welcome Mr :",name);
         console.log('====================================');
-      res.status(200).json({ message: `Welcome to our website, Mr./Ms. ${name}` });
+      res.status(200).json({ message: `Welcome to our Channel : ${name}` , NameValid : name});
     } else {
       res.status(400).json({ message: 'Name is required' });
     }
